bdd6fb3e4f0baea0bc28ce3eb166598d7a90821e
JydieCPT <jydiecpt@gmail.com>
2025-11-16 18:39:23 +0200

    Added theme toggle code to header.jsx file.

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,12 +1,21 @@
-import styles from "./Header.module.css";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../../context/ThemeContext";
+import styles from "./Header.module.css";
+
 export default function Header() {
+  // ✅ Destructure theme and toggleTheme from ThemeContext
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
     <header className={styles.appHeader}>
       <h1>
-        {" "}
         <Link to="/">🎙️ Podcast App</Link>
       </h1>
+
+      <button className={styles.themeToggle} onClick={toggleTheme}>
+        {theme === "light" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+      </button>
     </header>
   );
 }
